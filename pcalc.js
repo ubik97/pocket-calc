1dbe9c28aa954986f38de52085adc1dbd174d91f
ubik97 <43785266+ubik97@users.noreply.github.com>
2019-01-04 11:12:00 -0500

    Update pcalc.js

--- pcalc.js
+++ pcalc.js
@@ -56,171 +56,161 @@ document.getElementById('output').innerHTML = input;
 }
 
 function number2() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "2";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "2";
   input = input.substring(0,9);
   } else {
     input = input + "2";
   }
+
 }
 
 document.getElementById('output').innerHTML = input;
 }
 
 function number3() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "3";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "3";
   input = input.substring(0,9);
   } else {
     input = input + "3";
   }
-  }
+ }
 
   document.getElementById('output').innerHTML = input;
 }
 
 function number4() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "4";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "4";
   input = input.substring(0,9);
   } else {
     input = input + "4";
   }
-}
+
 
 document.getElementById('output').innerHTML = input;
 }
 
 function number5() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "5";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "5";
   input = input.substring(0,9);
   } else {
     input = input + "5";
   }
-}
+
 
 document.getElementById('output').innerHTML = input;
 }
 
 function number6() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "6";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "6";
   input = input.substring(0,9);
   } else {
     input = input + "6";
   }
-}
 
 document.getElementById('output').innerHTML = input;
 }
 
 function number7() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "7";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "7";
   input = input.substring(0,9);
   } else {
     input = input + "7";
   }
-}
+
 
 document.getElementById('output').innerHTML = input;
 }
 
 function number8() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "8";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "8";
   input = input.substring(0,9);
   } else {
     input = input + "8";
   }
-}
+
 
 document.getElementById('output').innerHTML = input;
 }
 
 function number9() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "9";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "9";
   input = input.substring(0,9);
   } else {
     input = input + "9";
   }
-  }
+
 
   document.getElementById('output').innerHTML = input;
 }
 
 function number0() {
-  if (status="dirty") {
-    input = "";
+  if (status != "clean") {
+    input = "0";
     document.getElementById('output').innerHTML = input;
-    status= "clean";
-  }
-
-  if (status!="dirty") {
+    status = "clean";
+  } else {
+    console.log(status);
   if (input.length>=9) {
   input = input + "0";
   input = input.substring(0,9);
   } else {
     input = input + "0";
-  }
   }
 
   document.getElementById('output').innerHTML = input;
