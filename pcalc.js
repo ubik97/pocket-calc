816e6c379a66ae1b13443604a239a83bda9348ea
John Turner <ubik642@gmail.com>
2019-01-04 10:49:31 -0500

    created addition, still need to connect numbers 2-0

--- pcalc.js
+++ pcalc.js
@@ -1,117 +1,235 @@
 var input = "";
 var p = document.getElementById('output')
 var digcount = input.length;
+var addnum1;
+var optype;
+var sum;
+var status = "clean";
 
 function addition() {
-  
+  addnum1 = input;
+  console.log(addnum1);
+  input = "";
+  console.log(addnum1);
+  console.log("fuck");
+  optype = 1;
+}
+
+function equals() {
+  switch(optype) {
+    case 1:
+      status = "dirty";
+      document.getElementById('output').innerHTML = eval(Number(input) + Number(addnum1));
+      console.log(status);
+      break;
+
+    case 2:
+
+
+
+  }
 }
 
 
-            //BASIC KEYPAD INPUT FUNCTIONS
+
+//console.log(eval('2 + 2')); will log 4 to the console
+
+
+//BASIC KEYPAD INPUT FUNCTIONS
 function number1() {
-  if (input.length>10) {
+  if (status != "clean") {
+    input = "1";
+    document.getElementById('output').innerHTML = input;
+    status = "clean";
+  } else {
+    console.log(status);
+  if (input.length>=9) {
   input = input + "1";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "1";
   }
+
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number2() {
-  if (input.length>10) {
-    input = input + "2";
-    input = input.substring(0,11);
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
+  input = input + "2";
+  input = input.substring(0,9);
   } else {
     input = input + "2";
   }
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number3() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "3";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "3";
   }
-document.getElementById('output').innerHTML = input;
+  }
+
+  document.getElementById('output').innerHTML = input;
 }
 
 function number4() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "4";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "4";
   }
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number5() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "5";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "5";
   }
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number6() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "6";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "6";
   }
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number7() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "7";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "7";
   }
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number8() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "8";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "8";
   }
+}
+
 document.getElementById('output').innerHTML = input;
 }
 
 function number9() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "9";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "9";
   }
-document.getElementById('output').innerHTML = input;
+  }
+
+  document.getElementById('output').innerHTML = input;
 }
 
 function number0() {
-  if (input.length>10) {
+  if (status="dirty") {
+    input = "";
+    document.getElementById('output').innerHTML = input;
+    status= "clean";
+  }
+
+  if (status!="dirty") {
+  if (input.length>=9) {
   input = input + "0";
-  input = input.substring(0,11);
+  input = input.substring(0,9);
   } else {
     input = input + "0";
   }
-document.getElementById('output').innerHTML = input;
+  }
+
+  document.getElementById('output').innerHTML = input;
 }
 
 function dec() {
-  if (input.length>10) {
+  if (input.length>=9) {
   input = input + ".";
-  input = input.substring(0,10);
+  input = input.substring(0,9);
   } else {
     input = input + ".";
   }
@@ -119,6 +237,6 @@ document.getElementById('output').innerHTML = input;
 }
 
 function reset() {
-  input = " ";
+  input = "";
   document.getElementById('output').innerHTML = input;
 }
